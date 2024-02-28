e3a15de1cc28a7472aa8ebdcb1ea49bb422c0b57
Asadullah246 <asadm2258@gmail.com>
2024-02-28 11:34:49 +0600

    updated code

--- next.config.js
+++ next.config.js
@@ -1,8 +1,34 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     appDir: false,
+//   },
+// };
+
+// module.exports = nextConfig;
+
+const webpack = require('webpack');
+
 const nextConfig = {
   experimental: {
     appDir: false,
   },
+  webpack: (config, { isServer }) => {
+    // Add the ProvidePlugin only in the client-side build
+    if (!isServer) {
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery',
+          'window.jQuery': 'jquery'
+        })
+      );
+    }
+
+    return config;
+  }
 };
 
 module.exports = nextConfig;
+ 
+
