d86452dace2546c2c25969ff5a432d74fb9bfc09
Asadullah246 <asadm2258@gmail.com>
2024-07-09 10:31:19 +0600

    updated api

--- components/shared/Api.js
+++ components/shared/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const base = "http://localhost:5000";
-// export const base = "https://backend.upturnidea.com"; 
+// export const base = "http://localhost:5000";
+export const base = "https://backend.upturnidea.com";
 
 
 // admin routes -----------------------------------------------------------------------------------
